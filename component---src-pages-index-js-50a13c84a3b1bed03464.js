3c959fbbf43e8ca3f02686ae8b404e2fe791bb72
JoachimAA <joachimarthey@hotmail.co.uk>
2021-01-16 17:03:26 +0000

    Updates

--- component---src-pages-index-js-50a13c84a3b1bed03464.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{RXBc:function(e,n,t){"use strict";t.r(n);var o=t("q1tI"),r=t.n(o),a=function(){return r.a.createElement("div",null,"my starter component test 23")},s={color:"#232129",padding:"96px",fontFamily:"-apple-system, Roboto, sans-serif, serif"};n.default=function(){return o.createElement("main",{style:s},o.createElement(a,null))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-50a13c84a3b1bed03464.js.map
