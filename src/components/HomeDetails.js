9cc911d15245fface956134b4f298d52bffff6cc
JoachimAA <joachimarthey@hotmail.co.uk>
2021-01-24 13:59:29 +0000

    add images

--- src/components/HomeDetails.js
+++ src/components/HomeDetails.js
@@ -1,16 +1,27 @@
 import React from "react";
 import style from "./homeDetails.module.css";
 import NavButton from "./common/NavButton";
+import MeSrc from "../images/me.png";
+import LogoSrc from "../images/logo.png";
+import "../css/typography.css";
 
 const HomeDetails = () => {
   return (
-    <div className={style.container}>
+    <div className={style.container} style={{ fontFamily: "Montserrat" }}>
       <div>
-        <div className={style.image}>picture box</div>
+        <div
+          style={{ backgroundImage: `url(${LogoSrc})` }}
+          className={style.image}
+        />
+
         <div className={style.imageText}>Get in touch</div>
       </div>
       <div>
-        <div className={style.image}>picture box</div>
+        <div
+          style={{ backgroundImage: `url(${MeSrc})` }}
+          className={style.image}
+        />
+
         <div className={style.imageText}>
           <NavButton to={"/about"} name={"About Lexie"} />
         </div>
--- src/components/HomeTitle.js
+++ src/components/HomeTitle.js
@@ -1,10 +1,17 @@
 import React from "react";
 import style from "./homeTitle.module.css";
 import "../css/typography.css";
+import BannerSrc from "../images/banner.png";
 
 const HomeTitle = () => {
   return (
-    <div style={{ fontFamily: "DancingScript" }} className={style.name}>
+    <div
+      style={{
+        fontFamily: "DancingScript",
+        backgroundImage: `url(${BannerSrc})`,
+      }}
+      className={style.name}
+    >
       Arthey Paper Designs
     </div>
   );
