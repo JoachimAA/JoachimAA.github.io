d107f2ade878b6b301017bab3e9adf28fae837c5
JoachimAA <joachimarthey@hotmail.co.uk>
2021-01-28 20:34:08 +0000

    add coming soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,34 @@ import HomeTitle from "../components/HomeTitle";
 import HomeDetails from "../components/HomeDetails";
 import Footer from "../components/Footer";
 import "../css/typography.css";
+import LogoSrc from "../images/logo.svg";
 
 const IndexPage = () => {
   return (
-    <main style={{ fontFamily: "Montserrat" }}>
+    <main style={{ fontFamily: "Montserrat", backgroundColor: "#f6f6f6" }}>
       <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          alignItems: "center",
+          fontSize: "68px",
+          height: "35vh",
+        }}
+      >
+        Coming soon...
+      </div>
+      <div
+        style={{
+          backgroundImage: `url(${LogoSrc})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain",
+          backgroundPosition: "center",
+          height: "50vh",
+        }}
+      />
+      <div style={{ backgroundColor: "#f6f6f6", height: "12vh" }} />
+      {/* <div
         style={{ height: "100vh", display: "flex", flexDirection: "column" }}
       >
         <Header />
@@ -17,7 +40,7 @@ const IndexPage = () => {
         </div>
       </div>
       <HomeDetails />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 };
